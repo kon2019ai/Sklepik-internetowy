714e6905f39459488645cc7bfd53c0ef113c28a4
kon2019ai <k0sm0@p.pl>
2019-01-09 03:34:44 +0100

    First commit

---
+++ AplikacjaInternetowa/test.py
@@ -0,0 +1,4 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+med = os.path.join(BASE_DIR, 'products')
+print(med)
---
+++ SklepInternetowy/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SklepInternetowyConfig(AppConfig):
+    name = 'SklepInternetowy'
